65b5f8e23492b3ee5940e0719ef5d55859993961
Pablo-778 <becarioit@sabor-espana.com>
2025-07-28 14:35:48 +0200

    BREAKING CHANGE: autentificacion

--- app.py
+++ app.py
@@ -3,10 +3,72 @@ from extraer_tabla import procesar_pdf
 import threading   
 import subprocess
 import time
+import os
+import requests
+from urllib.parse import urlencode
 
 st.set_page_config(page_title="Convertidor PDF → Excel", layout="centered")
-st.title("Convertidor de PDF a Excel")
 
+# Variables de entorno
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+TENANT_ID = os.environ.get("TENANT_ID")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
+
+AUTHORITY = f"https://login.microsoftonline.com/72ab3d9f-2e5d-436e-b1ad-97bc92c27295/oauth2/v2.0"
+SCOPE = "User.Read"
+
+def get_auth_url():
+    params = {
+        "client_id": CLIENT_ID,
+        "response_type": "code",
+        "redirect_uri": REDIRECT_URI,
+        "response_mode": "query",
+        "scope": SCOPE,
+        "state": "12345"  # Para simplificar, luego mejorarlo
+    }
+    url = f"{AUTHORITY}/authorize?{urlencode(params)}"
+    return url
+
+# --- LOGIN ---
+
+query_params = st.experimental_get_query_params()
+
+if "code" not in query_params:
+    st.title("Iniciar sesión")
+    auth_url = get_auth_url()
+    st.markdown(f"[Inicia sesión con Microsoft 365]({auth_url})")
+    st.stop()  # No mostrar nada más hasta que se inicie sesión
+
+# --- CALLBACK ---
+
+else:
+    code = query_params["code"][0]
+
+    token_url = f"{AUTHORITY}/token"
+    data = {
+        "client_id": CLIENT_ID,
+        "scope": SCOPE,
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+        "grant_type": "authorization_code",
+        "client_secret": CLIENT_SECRET
+    }
+    response = requests.post(token_url, data=data)
+    tokens = response.json()
+
+    if "access_token" not in tokens:
+        st.error("Error obteniendo token:")
+        st.write(tokens)
+        st.stop()
+
+    access_token = tokens["access_token"]
+
+    st.success("¡Login correcto!")
+    st.write(f"Token de acceso: {access_token[:40]}...")
+
+
+st.title("Convertidor de PDF a Excel")
 st.markdown("Sube un archivo PDF de factura para convertirlo automáticamente a Excel.")
 
 # Subida del archivo PDF
