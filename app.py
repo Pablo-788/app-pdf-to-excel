597f0a682d703ac9bc8c2d7344df9aff6e80ba0c
Pablo-778 <becarioit@sabor-espana.com>
2025-07-30 07:57:49 +0200

    Fix:scope

--- app.py
+++ app.py
@@ -14,12 +14,11 @@ CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 TENANT_ID = os.environ.get("TENANT_ID")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
-SCOPE = "openid offline_access https://graph.microsoft.com/User.Read"
 
 print("REDIRECT_URI:", REDIRECT_URI)
 
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0"
-SCOPE = "User.Read"
+SCOPE = "openid offline_access https://graph.microsoft.com/User.Read"
 
 def get_auth_url():
     params = {
